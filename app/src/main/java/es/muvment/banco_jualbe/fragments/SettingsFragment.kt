1a0144203a0f7e4503e7803ac9c8b91237aa0e3e
muvbit <alberola@outlook.com>
2025-01-15 10:07:50 +0100

    - ACTIVITIES pasadas a FRAGMENTS con navigation 3.0. - Drawer incluido pero no responde a los clicks

--- app/src/main/java/es/muvment/banco_jualbe/fragments/SettingsFragment.kt
+++ app/src/main/java/es/muvment/banco_jualbe/fragments/SettingsFragment.kt
@@ -1,20 +1,12 @@
 package es.muvment.banco_jualbe.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.preference.PreferenceFragmentCompat
 import es.muvment.banco_jualbe.R
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
     }
-
 }
